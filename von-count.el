f930c4ba05357337a1b69b011c6d27f0654d2bba
Gav Massingham <gavmassingham@gmail.com>
2026-09-19 12:55:09 +0100

    Changes from some time ago

--- von-count.el
+++ von-count.el
@@ -70,7 +70,13 @@ The position along the bar in proportion to the amount of
 (defvar-local von-count-is-bar nil
   "True in the Von Count bar buffer.")
 
-(defun von-count-display-bar-buffer(&optional pos)
+(defun von-count-get-bar-window ()
+  (if-let* ((bar-buffer-window
+             (window-parameter (selected-window) 'von-count-bar-buffer-window)))
+      (and (window-live-p bar-buffer-window)
+    bar-buffer-window)))
+
+(defun von-count-display-bar-buffer (&optional pos)
   "Initialize and display the Von Count buffer and bar.
 
 If POS is passed, this specifies the position from which counting of
@@ -78,38 +84,42 @@ If POS is passed, this specifies the position from which counting of
 below the window containing the buffer where Von Count mode was
 enabled. The bar is resized automatically if the window width changes."
   (interactive)
-  (and (or von-count-bar-buffer von-count-is-parent)
-       (von-count-remove-bar-buffer))
-  (setq-local von-count-bar-buffer
-              (get-buffer-create (concat "Von Count " (buffer-name)))
-              von-count-is-parent t
-              von-count-word-count-delta 0
-              von-count-last-word-count-delta 0)
-  (and pos (message "Pos is %s" pos))
-  (von-count-store-file-word-count (or pos))
-  (let ((display-buffer-alist
-         `((t
-            (display-buffer-in-atom-window)
-            (reusable-frames . visible)
-            (dedicated . t)
-            (window-height . (body-lines . 2))
-            (preserve-size . (nil . t))
-            (window-parameters ((no-other-window . t)
-                                (delete-window . t)
-                                (no-delete-other-window . t)))
-            ))))
-    (display-buffer von-count-bar-buffer))
-  (let ((parent-buffer (current-buffer)))
-    (with-current-buffer von-count-bar-buffer
-      (setq-local von-count-parent-buffer parent-buffer 
-                  von-count-is-bar t)
-      (von-count-bar-buffer-mode-line)
-      (setq cursor-type nil)
-      (setq buffer-read-only t)
-      (add-hook 'window-size-change-functions #'von-count-bar-maybe-redraw 0 t)))
-  (von-count-bar-buffer-setup (von-count-bar-get-length))
-  (add-hook 'after-change-functions #'von-count-after-change-function 0 t)
-  (add-hook 'kill-buffer-hook #'von-count-killed-remove-bar-buffer 0 t))
+  (make-local-variable 'von-count-target)
+  (let* ((bar-buffer-window (von-count-get-bar-window))
+         (bar-buffer-name (concat "von Count " (buffer-name))))
+    (message "Von Count bar buffer window: %s" bar-buffer-window)
+    (setq-local von-count-bar-buffer
+                (get-buffer-create bar-buffer-name)
+                von-count-is-parent t
+                von-count-word-count-delta 0
+                von-count-last-word-count-delta 0)
+    (von-count-store-file-word-count (or pos))
+    (if bar-buffer-window
+        (set-window-buffer bar-buffer-window von-count-bar-buffer)
+      (let ((display-buffer-alist
+             `((t
+                (display-buffer-in-atom-window)
+                (window . ,(selected-window))
+                (window-height . (body-lines . 2))
+                (preserve-size . (nil . t))
+                (reusable-frames . visible)
+                (window-parameters ((no-other-window . t)
+                                    (delete-window . t)
+                                    (no-delete-other-window . t)))))))
+        (display-buffer von-count-bar-buffer)))
+    (set-window-parameter (selected-window) 'von-count-bar-buffer-window (get-buffer-window von-count-bar-buffer))
+    (message "Window: %s %s %s" von-count-bar-buffer (get-buffer-window von-count-bar-buffer) (window-parameter (selected-window) 'von-count-bar-buffer-window))
+    (let ((parent-buffer (current-buffer)))
+      (with-current-buffer von-count-bar-buffer
+        (setq-local von-count-parent-buffer parent-buffer 
+                    von-count-is-bar t)
+        (von-count-bar-buffer-mode-line)
+        (setq cursor-type nil)
+        (setq buffer-read-only t)
+        (add-hook 'window-size-change-functions #'von-count-bar-maybe-redraw 0 t)))
+    (von-count-bar-buffer-setup (von-count-bar-get-length))
+    (add-hook 'after-change-functions #'von-count-after-change-function 0 t)
+    (add-hook 'kill-buffer-hook #'von-count-killed-remove-bar-buffer 0 t)))
 
 (defun von-count-restart-count-from-point()
   "Restart von-count with the initial word count set to those in the region
@@ -119,6 +129,7 @@ between point-min and point."
 
 (defun von-count-remove-bar-buffer()
   "Remove Von Count bar and clean up."
+  (interactive)
   (remove-hook 'after-change-functions #'von-count-after-change-function t)
   (remove-hook 'kill-buffer-hook #'von-count-killed-remove-bar-buffer t)
   (when (and von-count-bar-buffer (buffer-live-p von-count-bar-buffer))
@@ -127,34 +138,49 @@ between point-min and point."
         (walk-window-subtree #'(lambda(window)
                                  (set-window-parameter window 'window-atom nil))
                              (window-atom-root) 'any)
-               (kill-buffer))))
+        (kill-buffer))))
   (setq-local von-count-bar-buffer nil
               von-count-is-parent nil))
 
-(defun von-count-killed-remove-bar-buffer()
+(defun von-count-killed-remove-bar-buffer ()
   "If this buffer is parent of a Von Count bar buffer, remove the bar
 buffer if this buffer is killed."
   (and von-count-is-parent
     (von-count-remove-bar-buffer)))
+
+(defun von-count-refresh-bar-buffer-window ()
+  (interactive)
+  (if-let* ((bar-window (window-parameter (selected-window) 'von-count-bar-buffer-window))
+            (bar-buffer von-count-bar-buffer))
+      (set-window-buffer bar-window bar-buffer)))
+
+(remove-hook 'buffer-list-update-hook #'von-count-refresh-bar-buffer-window t)
+(add-hook 'buffer-list-update-hook #'von-count-refresh-bar-buffer-window 0 t)
 
 (defmacro von-count-wrapper (parent-or-bar &rest body)
   "Wrapper that makes sure BODY is called in the correct buffer.
 
 PARENT-OR-BAR should be one of either `'parent' or `bar', and specifies
 in which buffer BODY is called."
-  (if (equal parent-or-bar 'bar)
-      `(with-current-buffer (or (and von-count-is-bar (current-buffer)) von-count-bar-buffer)
-     ,@body)
-    `(with-current-buffer (or (and von-count-is-parent (current-buffer)) von-count-parent-buffer)
+  (let* ((sym (symbol-name parent-or-bar))
+         (which (intern-soft (concat "von-count-is-" sym)))
+         (buf (intern-soft (concat "von-count-" sym "-buffer"))))
+    
+  `(with-current-buffer (or (and ,which (current-buffer)) ,buf)
      ,@body)))
+
+(defmacro von-count-with-bar-buffer (&rest body)
+  `(von-count-wrapper bar ,@body))
+
+(defmacro von-count-with-parent-buffer (&rest body)
+  `(von-count-wrapper parent ,@body))
 
 (defun von-count-bar-buffer-setup (bar-length)
   "Create a bar of BAR-LENGTH copies of `von-count-segment-char' in `von-count-bar-buffer'."
   (let ((inhibit-modification-hooks 'inhibit)
         (done-pos (von-count-bar-get-done-pos))
         (bar (propertize (make-string bar-length von-count-bar-segment-char) 'face 'von-count-bar)))
-    (von-count-wrapper
-     bar
+    (von-count-with-bar-buffer
      (von-count-bar--set-colors bar-length done-pos bar)
      (let ((inhibit-read-only t))
        (erase-buffer)
@@ -168,8 +194,7 @@ far.  The ‘done’ portion is coloured with `von-count-done-color’, and the
 ‘todo’ portion with `von-count-todo-color’.  Each segment of the bar is
 the character `von-count-segment-char’.  The bar has an associated face:
 `von-count-bar’."
-  (von-count-wrapper
-   bar
+  (von-count-with-bar-buffer
    (von-count-bar--set-colors
     (von-count-bar-get-length)
     (von-count-bar-get-done-pos))))
@@ -194,7 +219,7 @@ used its length should be the same as the value of BAR-LENGTH."
          (set-text-properties start done-pos
                               `(face (von-count-bar (:foreground ,von-count-done-color))) bar))))
 
-(defun von-count-after-change-function(beg end len)
+(defun von-count-after-change-function (beg end len)
   "if words have been added to the `von-count-parent-buffer', update the
 bar."
   (and (not (= (von-count-get-delta) von-count-last-word-count-delta))
@@ -202,13 +227,12 @@ bar."
        (von-count-bar-buffer-mode-line)
        (von-count-bar-set-colors)))
 
-(defun von-count-bar-maybe-redraw(&optional frame)
+(defun von-count-bar-maybe-redraw (&optional frame)
   "Redraw the Von Count bar if needed.
 
 If the width of the window where `von-count-bar-buffer’ is displayed has
 changed, redraw the bar."
-  (von-count-wrapper
-   bar
+  (von-count-with-bar-buffer
    (and (not (=
               (von-count-bar-get-length)
               (or von-count-bar-last-length
@@ -216,19 +240,29 @@ changed, redraw the bar."
         (setq-local von-count-bar-last-length von-count-bar-length)
         (von-count-bar-buffer-setup von-count-bar-length))))
 
+(defun von-count-bar-buffer-p ()
+   (and von-count-is-parent von-count-bar-buffer))
+
 (defun von-count-bar-get-done-pos ()
   "Calculate ‘done’ position on a Von Count bar.
 
 This is the character position that corresponds to the proportion of
  `von-count-target’ words typed so far."
-  (von-count-wrapper
-   bar
+  (von-count-with-bar-buffer
+   (let* ((worked  (/ (* 1000 (von-count-get-target)) (von-count-bar-get-length))))
+     (setq-local von-count-bar-done-pos (/ (* 1000 (von-count-get-delta)) worked)))))
+
+(defun von-count-bar-get-done-pos ()
+  "Calculate ‘done’ position on a Von Count bar.
+
+This is the character position that corresponds to the proportion of
+ `von-count-target’ words typed so far."
+  (von-count-with-bar-buffer
    (let* ((worked  (/ (* 1000 (von-count-get-target)) (von-count-bar-get-length))))
      (setq-local von-count-bar-done-pos (/ (* 1000 (von-count-get-delta)) worked)))))
 
 (defun von-count--get-word-count(&optional pos)
-  (von-count-wrapper
-   parent
+  (von-count-with-parent-buffer
    (count-words (point-min) (or pos (point-max)))))
 
 (defun von-count-store-file-word-count(&optional pos)
@@ -236,14 +270,12 @@ This is the character position that corresponds to the proportion of
 
 The optional POS specifies the point at which counting should stop. By
 default the word count is of the whole buffer"
-  (von-count-wrapper
-   parent
-   (message "Inital word-count is %s" (setq-local von-count-initial-word-count (von-count--get-word-count pos)))))
+  (von-count-with-parent-buffer
+   (setq-local von-count-initial-word-count (von-count--get-word-count pos))))
 
 (defun von-count-bar-get-length()
   "Return the length of the bar."
-  (von-count-wrapper
-   bar
+  (von-count-with-bar-buffer
    (setq-local von-count-bar-length
                (window-max-chars-per-line
                 (get-buffer-window (current-buffer) t) 'von-count-bar))
@@ -253,7 +285,7 @@ default the word count is of the whole buffer"
   "Return the difference between the word count now, and the previous word count.
 
 If the difference is negative, return 0."
-  (von-count-wrapper parent
+  (von-count-with-parent-buffer
    (and (< (setq-local von-count-word-count-delta
                        (- (von-count--get-word-count) von-count-initial-word-count)) 0)
         ;; If we've shrunk the file to fewer words than it initally had,
@@ -264,12 +296,12 @@ If the difference is negative, return 0."
    von-count-word-count-delta))
 
 (defun von-count-get-target()
-  (von-count-wrapper parent
+  (von-count-with-parent-buffer
   von-count-target))
 
 (defun von-count-bar-buffer-mode-line()
   "Set a simple mode-line for the Von Count bar buffer."
-  (von-count-wrapper bar
+  (von-count-with-bar-buffer
   (setq mode-line-format
           (format-mode-line
            '((:eval (format "%s  %s/%s"
@@ -278,6 +310,12 @@ If the difference is negative, return 0."
                             (von-count-get-target))))))
     (force-mode-line-update))
   t)
+
+(defun von-count-mode-toggle ()
+  (interactive)
+  (if von-count-mode
+       (von-count-display-bar-buffer)
+    (von-count-remove-bar-buffer)))
 
 ;;;###autoload
 (define-minor-mode von-count-mode
@@ -288,11 +326,8 @@ typed in a buffer. It shows a bar that indicates how many words of a
 specified target number have been entered. The buffer that Von Count
 mode watches is the buffer that is current when Von Count mode is
 enabled."
-  :global t
   :lighter nil
-  (if von-count-mode
-    (von-count-display-bar-buffer)
-    (von-count-remove-bar-buffer)))
+  (von-count-mode-toggle))
 
 (provide 'von-count)
 
